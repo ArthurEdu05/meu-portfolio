35eeb4f2730552d58b52d028daa7238e57e609df
ArthurEdu05 <arthuredu2005@gmail.com>
2025-07-04 11:57:11 -0300

    feat: add action to contact button

--- src/pages/Home/sections/Hero/Hero.jsx
+++ src/pages/Home/sections/Hero/Hero.jsx
@@ -21,7 +21,8 @@ export default function Hero() {
           </p>
 
           <div className="heroButtons">
-            <a href="#!" className="BtnContato">Entre em contato</a>
+            <a href="mailto:arthuredu2005@gmail.com?subject=Interesse%20e%20oportunidade%20de%20colaboração%20-%20Portfólio&body=Olá%20Arthur,%0D%0A%0D%0AAcessei%20seu%20portfólio%20recentemente%20e%20fiquei%20bastante%20interessado(a)%20no%20seu%20trabalho.%20Gostaria%20de%20conversar%20melhor%20sobre%20uma%20possível%20colaboração%20ou%20oportunidade%20profissional.%0D%0A%0D%0AQuando%20estiver%20disponível,%20por%20favor,%20me%20retorne%20este%20e-mail%20para%20seguirmos%20com%20a%20conversa.%0D%0A%0D%0ADesde%20já,%20agradeço%20pela%20atenção!%0D%0A%0D%0AAtenciosamente,%0D%0A[Seu%20nome]"
+            className="BtnContato">Entre em contato</a>
             <a href={cvFile} className="BtnDownloadCv" target="_blank" rel="noopener noreferrer">
               Download CV
             </a>
